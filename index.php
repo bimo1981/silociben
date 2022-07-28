96f9a398676320267b5dfc193361648e6ee59dd9
123 <3122gmail.com>
2022-07-28 09:55:17 +0300

    10

--- index.php
+++ index.php
@@ -1,19 +1,21 @@
 <!DOCTYPE html>
 <html lang="ru">
-<head></head>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= 'hello ?'; ?></title>
+</head>
 <body>
-    
-<?php
-
-$number1 = 15;
-$number2 = "12";
-$number3 = (integer)$number2;
-
-echo gettype($number3)
-
-?>
+    <?php
+    if(mt_rand(0,1)){
+        ?>
+        <div style="color: blue;">Синий текст</div>
+        <?php 
+    } else {
+        ?>
+        <div style="color: red;">Красный текст</div>
+        <?php
+    }
+    ?>
 </body>
-
 </html>
-
-
